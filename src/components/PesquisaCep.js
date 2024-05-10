cf18f1a13b519cf441f056f11f882e2b1798bbd3
Luis Paulo <luispaulolpsn@gmail.com>
2024-05-10 12:49:12 -0300

    estilo do formulario

--- src/components/PesquisaCep.js
+++ src/components/PesquisaCep.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './meuEstilo.css';
 
 const PesquisaCep = () => {
   const [cep, setCep] = useState('');
   const [address, setAddress] = useState(null);
   const [error, setError] = useState(null);
 
-  const handleSearch = async () => {
+  const buscaCep = async () => {
     try {
       const response = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
       setAddress(response.data);
@@ -18,28 +19,32 @@ const PesquisaCep = () => {
   };
 
   return (
-    <div>
-      <h2>Pesquisar CEP</h2>
-      <input
-        type="text"
-        value={cep}
-        onChange={(e) => setCep(e.target.value)}
-        placeholder="Digite o CEP"
-      />
-      <button onClick={handleSearch}>Pesquisar</button>
+    <div className="container">
+      <div className="left-column">
+        <h2>Pesquisar CEP</h2>
+        <input
+          type="text"
+          value={cep}
+          onChange={(e) => setCep(e.target.value)}
+          placeholder="Digite o CEP"
+          className="custom-input"
+        />
+        <button className="custom-button" onClick={buscaCep}>Pesquisar</button>
+      </div>
 
-      {error && <p>{error}</p>}
-
-      {address && (
-        <div>
-          <h3>Resultado:</h3>
-          <p>CEP: {address.cep}</p>
-          <p>Logradouro: {address.logradouro}</p>
-          <p>Bairro: {address.bairro}</p>
-          <p>Cidade: {address.localidade}</p>
-          <p>Estado: {address.uf}</p>
-        </div>
-      )}
+      <div className="right-column">
+        {error && <p>{error}</p>}
+        {address && (
+          <div>
+            <h3>Resultado:</h3>
+            <p>CEP: {address.cep}</p>
+            <p>Logradouro: {address.logradouro}</p>
+            <p>Bairro: {address.bairro}</p>
+            <p>Cidade: {address.localidade}</p>
+            <p>Estado: {address.uf}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
